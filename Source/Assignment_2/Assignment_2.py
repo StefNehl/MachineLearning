bcf03df4934ba4e9c080f5c0dcec5c80db39f270
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-02-28 22:50:46 +0100

    Added first plot

--- Source/Assignment_2/Assignment_2.py
+++ Source/Assignment_2/Assignment_2.py
@@ -1,4 +1,6 @@
 import csv
+
+import matplotlib.pyplot as plt
 from BasicStatistics import BasicStatistics
 
 dataSet = []
@@ -8,10 +10,28 @@ with open('gauss.csv', mode='r') as file:
     csvFile = csv.reader(file)
 
     for line in csvFile:
-        dataSet.append(line)
+        dataSet.append(float(line[0]))
 
 basicStatistics = BasicStatistics()
 
-print(basicStatistics.getMeadian(dataSet))
+mean = basicStatistics.getMean(dataSet)
+median = basicStatistics.getMedian(dataSet)
+variance = basicStatistics.getVariance(dataSet)
+standardDeviation = basicStatistics.getStandardDeviation(dataSet)
+standardizedDataSet = basicStatistics.standardizeDataSet(dataSet)
+
+length = len(standardizedDataSet)
+range = range(length)
+
+fig = plt.figure()
+fig.suptitle('Raw Data')
+plt.xlabel('Samples')
+plt.ylabel('Values')
+
+x = range
+y = dataSet
+
+plt.plot(x, y)
+plt.show()
 
 
