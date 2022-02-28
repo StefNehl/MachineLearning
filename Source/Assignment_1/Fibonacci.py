338be93fb9115772c47e2d76604906ed61f83795
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-02-28 21:30:09 +0100

    Added new Files

---
+++ Source/Assignment_1/Fibonacci.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+class Fibonacci:
+
+    def get_fibonacci_numbers(self, n):
+
+        if n < 0:
+            print("Input can't be lower then zero")
+            return 0
+        elif n == 0:
+            return 0
+        elif n == 1:
+            return 1
+
+        fibonacci_numbers = np.zeros(n)
+        for x in range(0, n):
+            calculatedValue = -1
+            if x == 0:
+                calculatedValue = 1
+            elif x == 1:
+                calculatedValue = 1
+            else:
+                calculatedValue = fibonacci_numbers[x-1] + fibonacci_numbers[x-2]
+
+            fibonacci_numbers[x] = calculatedValue
+            print('it: ' + str(x + 1) + ' v: ' + str(calculatedValue))
+
+        return fibonacci_numbers
+
+
+
+
