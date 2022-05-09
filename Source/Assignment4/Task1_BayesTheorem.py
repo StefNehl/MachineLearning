21947db4c5c53bfa4239eff1bf52e2201959e96a
Nehl-hub <68614152+Nehl-hub@users.noreply.github.com>
2022-05-09 10:59:09 +0200

    Added Bayes Theorem

--- Source/Assignment4/Task1_BayesTheorem.py
+++ Source/Assignment4/Task1_BayesTheorem.py
@@ -2,16 +2,36 @@ import numpy as np
 
 populationAustria = 9095538
 activeCases = 441098
-
 covTestSensitivity = 0.971
 covTestSpecific = 0.995
 
-pAInfected = activeCases / populationAustria
-pANotInfected = 1 - pAInfected
+#A...[infected, not-infected]
+#B...[positive, negative]
 
-pB = covTestSpecific
-pBA = covTestSensitivity
+pInfected = activeCases / populationAustria
+pNotInfected = 1 - pInfected
+
+pPositiveInfected = covTestSensitivity
+pNegativeNotInfected = covTestSensitivity
+
+pInfectedAndPositive = pInfected * pPositiveInfected
+pInfectedAndNegative = pInfected - pInfectedAndPositive
+
+pNotInfectedAndNegative = pNotInfected * pNegativeNotInfected
+pNegative = pInfectedAndNegative + pNotInfectedAndNegative
+
+pNotInfectedAndPositive = pNotInfected - pNotInfectedAndNegative
+pPositive = pInfectedAndPositive + pNotInfectedAndPositive
+
+pNegativeInfected = pInfectedAndNegative / pNegative
+pPositiveNotInfected = pNotInfectedAndPositive / pNotInfected
 
 #BayersTheorem
-pAB = pBA * pAInfected / pB
-print(pAB)
+pInfectedPositive = pPositiveInfected * pInfected / pPositive
+
+print("p(-|infected): " + str(pNegativeInfected))
+print("p(+|non-infected): " + str(pPositiveNotInfected))
+print("p(infected): " + str(pInfected))
+print("p(non-infected): " + str(pNotInfected))
+print("p(+): " + str(pPositive))
+print("p(infected|+): " + str(pInfectedPositive))
